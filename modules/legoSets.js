33905a2ad4b0d12b886431c818b28ec1308fa96f
tdvan2010 <tdvan@myseneca.ca>
2025-11-16 11:25:28 -0500

    Update legoSets.js

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -25,6 +25,7 @@ class LegoData {
             try {
                 const setData = require("../data/setData.json");
                 const themeData = require("../data/themeData.json");
+                this.themes = [...themeData];
 
                 setData.forEach(set => {
                     const themeObj = themeData.find(theme => theme.id == set.theme_id);
@@ -48,6 +49,16 @@ class LegoData {
             }
         });
     }
+    
+    getAllThemes() {
+    return new Promise((resolve, reject) => {
+        if (this.themes.length > 0) {
+            resolve(this.themes);
+        } else {
+            reject("no themes available");
+        }
+    });
+}
 
     getSetByNum(setNum) {
         return new Promise((resolve, reject) => {
@@ -59,6 +70,17 @@ class LegoData {
             }
         });
     }
+
+    getThemeById(id) {
+    return new Promise((resolve, reject) => {
+        const theme = this.themes.find(t => t.id == id);
+        if (theme) {
+            resolve(theme);
+        } else {
+            reject("unable to find requested theme");
+        }
+    });
+}
 
     getSetsByTheme(theme) {
         return new Promise((resolve, reject) => {
@@ -81,6 +103,19 @@ class LegoData {
             } else {
                 this.sets.push(newSet);
                 resolve(newSet);
+            }
+        });
+    }
+}
+
+deleteSetByNum(setNum) {            // 🔸 HÀM MỚI
+        return new Promise((resolve, reject) => {
+            const index = this.sets.findIndex(s => s.set_num == setNum);
+            if (index !== -1) {
+                this.sets.splice(index, 1);
+                resolve();
+            } else {
+                reject("set not found");
             }
         });
     }
